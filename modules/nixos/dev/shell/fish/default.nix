29115659e8b7237c93f2754bd666a64ccbbb6c6a
rice-cracker-dev <ricecracker2234@proton.me>
2025-03-21 22:45:30 +0700

    feat: removed sddm

--- modules/nixos/dev/shell/fish/default.nix
+++ modules/nixos/dev/shell/fish/default.nix
@@ -4,6 +4,10 @@
     command-not-found.enable = false;
 
     bash.interactiveShellInit = ''
+      if uwsm check may-start && uwsm select; then
+	      exec uwsm start default
+      fi
+
       if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
       then
         shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
