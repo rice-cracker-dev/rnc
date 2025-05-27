505305767e8704fe81bbc2fe9bfeec3329c2ddde
rice-cracker-dev <ricecracker2234@proton.me>
2025-05-27 15:51:42 +0700

    hyprpicker: wtf is a rum 🗣️🗣️🗣️🗣️

--- modules/home/hyprland/hyprpicker.nix
+++ modules/home/hyprland/hyprpicker.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   config.me = {
     packages = [pkgs.hyprpicker];
-    rums.programs.hyprland.settings.bind = ["super shift, c, exec, hyprpicker -a"];
+    rum.programs.hyprland.settings.bind = ["super shift, c, exec, hyprpicker -a"];
   };
 }
