c1863bcded72a523b3ed365de9c358c06caaf0ba
rice-cracker-dev <ricecracker2234@proton.me>
2025-09-05 23:06:57 +0700

    what

--- hosts/rc555/hardware-configuration.nix
+++ hosts/rc555/hardware-configuration.nix
@@ -8,33 +8,26 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "sdhci_pci" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/71f74cf9-0912-4f79-96b6-99a5c3091099";
+    { device = "/dev/disk/by-uuid/d1a37d50-61e3-480b-9fba-45fd99ecb650";
       fsType = "btrfs";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/F2CE-E788";
+    { device = "/dev/disk/by-uuid/A325-7C43";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
   swapDevices =
     [ { device = "/dev/disk/by-uuid/38c0261c-b114-4993-ba77-5ab6d0ef3f85"; }
+      { device = "/dev/disk/by-uuid/9e149fd7-1eb9-4e0c-bc2e-43ad869af63c"; }
     ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
