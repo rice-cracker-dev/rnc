37d4880acaa9ffcaf448143a9978d7ee7d532724
rice-cracker-dev <ricecracker2234@proton.me>
2025-11-25 18:15:00 +0700

    nix: change binary cache for cuda
    why can't nixos community have just one consistent fucking cache?

--- modules/dev/cuda.nix
+++ modules/dev/cuda.nix
@@ -1,8 +1,3 @@
 {
-  nix.settings = {
-    extra-substituters = ["https://nix-community.cachix.org"];
-    extra-trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
-  };
-
   nixpkgs.config.cudaSupport = true;
 }
--- modules/os/gaming.nix
+++ modules/os/gaming.nix
@@ -3,11 +3,6 @@
 in {
   imports = [pipewireLowLatency platformOptimizations];
 
-  nix.settings = {
-    extra-substituters = ["https://nix-gaming.cachix.org"];
-    extra-trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
-  };
-
   services.pipewire.lowLatency = {
     enable = true;
     quantum = 64;
