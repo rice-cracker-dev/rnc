2059b0ace3398e7083b214652bae7d6fb869a074
rice-cracker-dev <ricecracker2234@proton.me>
2026-01-09 14:29:00 +0700

    hyprland/binds: limit volume to 100%

--- modules/home/hyprland/bind.nix
+++ modules/home/hyprland/bind.nix
@@ -74,8 +74,8 @@ in {
     ];
 
     binde = [
-      ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
-      ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+      ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1"
+      ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- -l 1"
       ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
     ];
   };
