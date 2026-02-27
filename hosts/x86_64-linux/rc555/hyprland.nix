f74b577706b9b1b3c428df9ec0d086377945b223
rice-cracker-dev <ricecracker2234@proton.me>
2026-02-28 01:36:13 +0700

    hosts/rc555/hyprland: disable touchpad

--- hosts/x86_64-linux/rc555/hyprland.nix
+++ hosts/x86_64-linux/rc555/hyprland.nix
@@ -1,6 +1,15 @@
 {
-  config.home.rum.desktops.hyprland.settings.monitor = [
-    "eDP-1, preferred, 0x0, 1"
-    ", preferred, auto, 1, mirror, eDP-1"
-  ];
+  config.home.rum.desktops.hyprland.settings = {
+    monitor = [
+      "eDP-1, preferred, 0x0, 1"
+      ", preferred, auto, 1, mirror, eDP-1"
+    ];
+
+    device = [
+      {
+        name = "elan0412:01-04f3:3240-touchpad";
+        enabled = false;
+      }
+    ];
+  };
 }
