b471e54e3d3895b5197bd1968835ee73b9d0313c
rice-cracker-dev <ricecracker2234@proton.me>
2026-03-17 22:38:23 +0700

    desktop/fcitx: replace fcitx5-unikey with fcitx5-bamboo

--- modules/desktop/fcitx.nix
+++ modules/desktop/fcitx.nix
@@ -11,8 +11,8 @@ in {
     i18n.inputMethod = {
       type = "fcitx5";
       enable = true;
-      fcitx5.addons = with pkgs.qt6Packages; [
-        fcitx5-unikey
+      fcitx5.addons = with pkgs; [
+        fcitx5-bamboo
       ];
       fcitx5.waylandFrontend = true;
     };
