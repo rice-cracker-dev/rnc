3b59c37aab84a2149765ef15893acfc9dfe7b1b2
rice-cracker-dev <ricecracker2234@proton.me>
2026-04-13 17:20:13 +0700

    core/man: init

---
+++ modules/core/man.nix
@@ -0,0 +1,14 @@
+{pkgs, ...}: {
+  config = {
+    environment.systemPackages = with pkgs; [man-pages man-pages-posix];
+    documentation = {
+      dev.enable = true;
+      man = {
+        enable = true;
+        dev.enable = true;
+        man-db.enable = true;
+        mandoc.enable = false;
+      };
+    };
+  };
+}
