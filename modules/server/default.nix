97cd5ccc9a0528d41b2e62a28211a6c46cec15df
rice-cracker-dev <ricecracker2234@proton.me>
2026-05-01 18:22:08 +0700

    server: enable qemu-guest-tools

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -1,7 +1,17 @@
-{lib, ...}: let
-  inherit (lib) mkEnableOption;
+{
+  lib,
+  config,
+  ...
+}: let
+  inherit (lib) mkEnableOption mkIf;
+
+  cfg = config.server;
 in {
   options.server = {
     enable = mkEnableOption "server";
+  };
+
+  config = mkIf cfg.enable {
+    services.qemuGuest.enable = true;
   };
 }
