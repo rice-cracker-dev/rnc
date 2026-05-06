c1ffdb714532de4879fe735bd39b2a5d56a5c639
rice-cracker-dev <ricecracker2234@proton.me>
2026-05-07 03:38:00 +0700

    flake: update inputs

--- modules/desktop/wireshark.nix
+++ modules/desktop/wireshark.nix
@@ -4,7 +4,6 @@
   pkgs,
   ...
 }: let
-  inherit (pkgs) fetchFromGitLab;
   inherit (lib) mkIf;
   inherit (config) desktop;
 in {
@@ -13,19 +12,5 @@ in {
     home.packages = with pkgs; [wireshark];
 
     core.user.extraGroups = ["wireshark"];
-
-    # TODO: remove this after https://nixpk.gs/pr-tracker.html?pr=515269 reaches nixos-unstable
-    nixpkgs.overlays = [
-      (_: prev: {
-        wireshark = prev.wireshark.overrideAttrs (final: {
-          src = fetchFromGitLab {
-            repo = "wireshark";
-            owner = "wireshark";
-            tag = "v${final.version}";
-            hash = "sha256-Zvrwxjp4LK2J3QnxmPxKKrU01YHQvPyp54UWzeGNCjA=";
-          };
-        });
-      })
-    ];
   };
 }
