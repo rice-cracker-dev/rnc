0de4ab54ff01115b9ca9c151e132d2a54b420e5e
rice-cracker-dev <ricecracker2234@proton.me>
2026-05-13 16:45:16 +0700

    utility/eh: add config

--- modules/utility/eh.nix
+++ modules/utility/eh.nix
@@ -1,4 +1,9 @@
-{inputs', ...}: let
+{
+  inputs',
+  pkgs,
+  ...
+}: let
+  inherit (pkgs.writers) writeTOML;
   eh = inputs'.eh.packages.default;
 in {
   environment.systemPackages = [eh];
@@ -10,5 +15,11 @@ in {
     nid = "eh develop";
     nii = "eh info";
     niu = "eh update";
+  };
+
+  home.xdg.config.files."eh/config.toml".source = writeTOML "eh-config" {
+    impure = true;
+    commands.build.env.NIXPKGS_ALLOW_UNFREE = "1";
+    commands.run.env.NIXPKGS_ALLOW_UNFREE = "1";
   };
 }
