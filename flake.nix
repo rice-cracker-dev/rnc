f0338da86d0ad54725715cd48ed2d2fa662121d1
rice-cracker-dev <ricecracker2234@proton.me>
2026-05-31 23:29:36 +0700

    flake: update inputs
    rust maintainers think they deserve to live

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
     ricevim.url = "github:rice-cracker-dev/ricevim";
     nh.url = "github:nix-community/nh";
--- modules/core/ncro.nix
+++ modules/core/ncro.nix
@@ -16,26 +16,32 @@ in {
       upstreams = [
         {
           url = "https://cache.nixos.org";
+          public_key = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=";
           priority = 10;
         }
         {
           url = "https://nix-community.cachix.org";
+          public_key = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
           priority = 20;
         }
         {
           url = "https://cache.nixos-cuda.org";
+          public_key = "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=";
           priority = 30;
         }
         {
           url = "https://nix-gaming.cachix.org";
+          public_key = "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=";
           priority = 30;
         }
         {
           url = "https://hyprland.cachix.org";
+          public_key = "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=";
           priority = 30;
         }
         {
           url = "https://niri.cachix.org";
+          public_key = "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964=";
           priority = 30;
         }
       ];
