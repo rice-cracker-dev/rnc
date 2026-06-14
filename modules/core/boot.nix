852e59858a3470147de9c6ab389727cb17f951bd
rice-cracker-dev <ricecracker2234@proton.me>
2026-06-14 22:12:31 +0700

    core/boot: increase inotify limits

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -21,6 +21,12 @@ in {
           configurationLimit = 10;
         };
       };
+
+      kernel.sysctl = {
+        "fs.inotify.max_user_watches" = 524288;
+        "fs.inotify.max_user_instances" = 1024;
+        "fs.inotify.max_queued_events" = 32768;
+      };
     };
   };
 }
