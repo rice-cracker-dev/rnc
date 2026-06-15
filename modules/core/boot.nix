ddf6079f4d496cb53bec9e57934915b4e09ab55b
rice-cracker-dev <ricecracker2234@proton.me>
2026-06-15 12:16:59 +0700

    core/boot: double inotify limits

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -23,9 +23,9 @@ in {
       };
 
       kernel.sysctl = {
-        "fs.inotify.max_user_watches" = 524288;
-        "fs.inotify.max_user_instances" = 1024;
-        "fs.inotify.max_queued_events" = 32768;
+        "fs.inotify.max_user_watches" = 1048576;
+        "fs.inotify.max_user_instances" = 2048;
+        "fs.inotify.max_queued_events" = 65536;
       };
     };
   };
