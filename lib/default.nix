524916c68dbb4f79ef48ebb0a139237fd0473c26
rice-cracker-dev <ricecracker2234@proton.me>
2026-06-26 16:12:50 +0700

    lib/dag: init

--- lib/default.nix
+++ lib/default.nix
@@ -2,4 +2,5 @@
   filesystem = import ./filesystem.nix {inherit lib;};
   types = import ./types.nix {inherit lib;};
   generators = import ./generators {inherit lib;};
+  dag = import ./dag.nix {inherit lib;};
 }
